07b539ebef02857dbdc044c92daf98a1be27fbde
Rad Cirskis <nad2000@gmail.com>
2016-05-25 01:18:23 +1200

    Improved test data (less companies, less test users, + dedicated admin users)

--- ProCareHealthTimeSheetsLib/DbScripts/load_test.cmd
+++ ProCareHealthTimeSheetsLib/DbScripts/load_test.cmd
@@ -1,6 +1,8 @@
 SET SQLCMD="C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SQLCMD.EXE"
 SET SERVER=(localdb)\MSSQLLocalDB
+SET DB=TimeSheetDB
 
-%SQLCMD% -E -S %SERVER% -d TimeSheetDB -i CompaniesTestData.sql
-%SQLCMD% -E -S %SERVER% -d TimeSheetDB -i UsersTestData.sql
-%SQLCMD% -E -S %SERVER% -d TimeSheetDB -i genTestTimeSheetData.sql
+%SQLCMD% -E -S %SERVER% -d %DB% -Q "SET QUOTED_IDENTIFIER ON; DELETE FROM TimeSheetEntries; DELETE FROM UserCompany; DELETE FROM Users; DELETE FROM Companies;"
+%SQLCMD% -E -S %SERVER% -d %DB% -i CompaniesTestData.sql
+%SQLCMD% -E -S %SERVER% -d %DB% -i UsersTestData.sql
+%SQLCMD% -E -S %SERVER% -d %DB% -i genTestTimeSheetData.sql
